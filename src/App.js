66236cc8ae8ee3e5a083f1885810d194c5fd74b8
Anshul <anshul.programmingjobs@gmail.com>
2021-02-09 18:09:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import User from './User';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     {/* <h1>App Component</h1> */}
+     <User data={{name:'Anshul Shrivastava'}}/>
     </div>
   );
 }
